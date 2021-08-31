abcebfa5f7c0bde3b1421e042e5569bf556719ac
Andrem19 <7255591@gmail.com>
2021-08-31 13:47:24 +0100

    new extension

--- src/pages/AuthPage.js
+++ src/pages/AuthPage.js
@@ -1,48 +1,49 @@
-import React, {useState, useEffect, useContext} from 'react';
+import React, { useState, useEffect, useContext } from "react";
 import { withRouter } from "react-router-dom";
-import axios from 'axios'
-import { AuthContext } from '../context/AuthContext';
+import axios from "axios";
+import { AuthContext } from "../context/AuthContext";
 
 const AuthPage = (props) => {
-    const auth = useContext(AuthContext)
-    const [form, setForm] = useState ({
-        name: '', email: '', password: ''
-    })
-       
-    useEffect(() => {
-    window.M.updateTextFields()
-    }, []);
+  const auth = useContext(AuthContext);
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
 
-    const changeHandler = event => {
-        setForm({...form, [event.target.name]: event.target.value})
-    }
+  useEffect(() => {
+    window.M.updateTextFields();
+  }, []);
 
-    const registerHandler = async () => {
-        axios.post('http://localhost:5000/user/register', {...form})
-           .then(res => console.log(res.data));
-    }
+  const changeHandler = (event) => {
+    setForm({ ...form, [event.target.name]: event.target.value });
+  };
 
-    const loginHandler = async () => {
-        axios.post('http://localhost:5000/user/login', {...form})
-           .then((res) => {
-             auth.login(res.data.token, res.data.userId, res.data.userName);
-             localStorage.setItem("CC_Token", res.data.token);
-             props.setupSocket();
-             console.log("token: ", res.data.token)
-            });
-           console.log("userId: ", auth.userId)
-       }
+  const registerHandler = async () => {
+    axios
+      .post("http://localhost:5000/user/register", { ...form })
+      .then((res) => console.log(res.data));
+  };
 
-    return (
-        <div className="row">
+  const loginHandler = async () => {
+    axios.post("http://localhost:5000/user/login", { ...form }).then((res) => {
+      auth.login(res.data.token, res.data.userId, res.data.userName);
+      localStorage.setItem("CC_Token", res.data.token);
+      props.setupSocket();
+      console.log("token: ", res.data.token);
+    });
+    console.log("userId: ", auth.userId);
+  };
+
+  return (
+    <div className="row">
       <div className="col s6 offset-s3">
         <h1>Trade Organizer</h1>
         <div className="card blue darken-1">
           <div className="card-content white-text">
             <span className="card-title">Авторизация</span>
             <div>
-
-            <div className="input-field">
+              <div className="input-field">
                 <input
                   placeholder="Введите имя"
                   id="text"
@@ -51,7 +52,6 @@ const AuthPage = (props) => {
                   className="yellow-input"
                   value={form.name}
                   onChange={changeHandler}
-                  
                 />
                 <label htmlFor="name">Name</label>
               </div>
@@ -65,7 +65,6 @@ const AuthPage = (props) => {
                   className="yellow-input"
                   value={form.email}
                   onChange={changeHandler}
-                  
                 />
                 <label htmlFor="email">Email</label>
               </div>
@@ -79,26 +78,22 @@ const AuthPage = (props) => {
                   className="yellow-input"
                   value={form.password}
                   onChange={changeHandler}
-                  
                 />
                 <label htmlFor="email">Пароль</label>
               </div>
-
             </div>
           </div>
           <div className="card-action">
             <button
-              className="btn yellow darken-4" 
-              style={{marginRight: 10}}
+              className="btn yellow darken-4"
+              style={{ marginRight: 10 }}
               onClick={loginHandler}
-              
             >
               Войти
             </button>
             <button
               className="btn grey lighten-1 black-text"
               onClick={registerHandler}
-
             >
               Регистрация
             </button>
@@ -106,7 +101,7 @@ const AuthPage = (props) => {
         </div>
       </div>
     </div>
-    );
-}
+  );
+};
 
 export default withRouter(AuthPage);
